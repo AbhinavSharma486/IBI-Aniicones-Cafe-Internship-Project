ba272934beee7d7635f5b616fb9c59e68171d042
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-04-16 11:07:52 +0530

    adding tailwindcss to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
 
 function App() {
 
   return (
     <>
-      <h1>Cafe Management</h1>
+      <h1 className="text-3xl font-bold underline text-green-600">
+        Hello world!
+      </h1>
     </>
   );
 }
